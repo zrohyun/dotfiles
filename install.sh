59ed08ffebb7267ef0ed5c571161a288037cda3d
zrohyun <ryh2803@dgu.ac.kr>
2024-02-21 16:43:38 +0000

    [FIX] zsh autosuggestions url 수정

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ install_omz() {
     # zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     # zsh-autosuggestions
-    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     # powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
