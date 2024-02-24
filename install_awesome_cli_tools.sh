c84d180893e0e5c9b0fc1b64ecbfa23dc3071cbb
zrohyun <ryh2803@dgu.ac.kr>
2024-02-25 01:24:13 +0900

    [ADD] pyenv, deepl, TODO(docker)

--- install_awesome_cli_tools.sh
+++ install_awesome_cli_tools.sh
@@ -51,3 +51,42 @@ for tool in "${cli_tools[@]}"; do
     install_cli_tool $tool
     # echo "$tool installed successfully."
 done
+
+# python
+# pyenv install
+install_pyenv() {
+    # from 
+    # 1. https://jinmay.github.io/2019/03/16/linux/ubuntu-install-pyenv-1/
+    # 2. https://www.dedicatedcore.com/blog/install-pyenv-ubuntu/
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+        libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+        xz-utils tk-dev
+    
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+    # vi ~/.zshrc
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    source ~/.zshrc
+
+
+    git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    eval "$(pyenv virtualenv-init -)"
+    source ~/.zshrc
+}
+
+# container
+#TODO: automation -> shell or ansible
+install_docker() {
+    # from
+    # https://docs.docker.com/engine/install/ubuntu/
+    #TODO: automation docker install 
+    # -> when creating workstation instance dynamically
+}
+intall_minikube() {
+    # from
+    # https://minikube.sigs.k8s.io/docs/start/
+    #TODO: minikube install automation
+}
