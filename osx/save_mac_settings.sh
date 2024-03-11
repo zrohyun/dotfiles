82e3c46cf8d3444a14c1f2885f8e5ecf39ee8317
zrohyun <ryh2803@dgu.ac.kr>
2024-03-11 16:57:17 +0900

    [MOD] Update system setting

--- osx/save_mac_settings.sh
+++ osx/save_mac_settings.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # from: https://apple.stackexchange.com/questions/87619/where-are-keyboard-shortcuts-stored-for-backup-and-sync-purposes
 # cd $HOME/Library/Preferences
+# 설정이 바뀔때마다 가끔 실행시켜주기
 
 system_files=(
     "com.apple.symbolichotkeys.plist"
@@ -19,6 +20,7 @@ if command -v mackup &>/dev/null; then
     # mackup을 사용하여 백업할 일이 있으면 그냥 가끔 한번씩 mackup bakcup && mackup uninstall로
     # 현 상태만 save하는 방식을 취하는 게 좋다. 그렇지 않으면 symlink가 걸려있으면 동작하지 않는다.
     # echo "[REQUIRED] mackup backup && mackup uninstall"
-    # echo "mackup backup && mackup uninstall" && mackup backup && mackup uninstall
-    echo "" &>/dev/null
+    echo "mackup backup && mackup uninstall" && mackup backup && mackup uninstall
+    #TODO: VSCODE 설정을 mackup 혹은 symlink로 설정해놓아야하나..? (근데 code는 이미 github으로 백업이 되고 있기는 한데)
+    # echo "" &>/dev/null
 fi
