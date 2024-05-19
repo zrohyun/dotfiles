2364236861918dd5f753f596a433da54abafcdf5
zrohyun <ryh2803@dgu.ac.kr>
2024-05-20 00:46:47 +0900

    [ADD] krew(kubectl) setting

--- awesome_linux_cli_tools_install.sh
+++ awesome_linux_cli_tools_install.sh
@@ -107,3 +107,14 @@ intall_minikube() {
     # https://minikube.sigs.k8s.io/docs/start/
     # TODO: minikube install automation
 }
+install_krew() {
+    # FROM
+    # https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
+}
